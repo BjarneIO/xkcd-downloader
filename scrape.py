9217c42cfe23e62d81144044fdd298779f904991
Bjarne <83133360+BjarneIO@users.noreply.github.com>
2023-03-22 23:55:48 +0100

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -25,28 +25,33 @@ if comics_in_folder == latest_comic_number:
     session.close()
 
 # Download the comics
-for comic_num in range(comics_in_folder + 2, latest_comic_number + 1):
+for comic_num in range(comics_in_folder + 1, latest_comic_number + 1):
     
     res = session.get(f'{BASE_URL}/{comic_num}')
     if res.status_code != 200:
-        print(f'Skipped #{comic_num} (Status code {res.status_code})')
+        print(f'Skipped comic #{comic_num} (Status code {res.status_code})')
         continue
     
     try: # If this errors it is probably an interactive comic and we skip it
         img_src = re.search(r'<img src="(.*?)" title="', res.text).group(1)
         img_src = img_src.replace('//', 'https://')
     except AttributeError:
-        print(f'Skipped #{comic_num} (Interactive comic)')
+        print(f'Skipped comic #{comic_num} (Interactive comic)')
         continue
 
     comic_title = re.search(r'<div id="ctitle">(.*?)</div>', res.text).group(1)
     comic_title = re.sub(r'[\\/:*?"<>|]', '', comic_title) # Remove illegal file name characters
 
     # Download and save the image
-    comic_file_name = f'{comic_num} {comic_title}'
-    with open(os.path.join('comics', f'{comic_file_name}.jpg'), 'wb') as f:
-        f.write(session.get(img_src).content)
-
-    print(f'Downloaded {comic_file_name}')
+    try:
+        comic_file_name = f'{comic_num} {comic_title}'
+        with open(os.path.join('comics', f'{comic_file_name}.jpg'), 'wb') as f:
+            f.write(session.get(img_src).content)
+    except httpx.UnsupportedProtocol:
+        print(f'Skipped comic #{comic_num} (Unsupported protocol) (Probably an interactive comic)')
+        os.remove(os.path.join('comics', f'{comic_file_name}.jpg'))
+        continue
+    
+    print(f'Downloaded comic #{comic_file_name}')
     
 session.close()
