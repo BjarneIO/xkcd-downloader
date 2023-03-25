dff99df6a9cdd6372fa0e7e7e621e4d7fcf880ab
Bjarne <83133360+BjarneIO@users.noreply.github.com>
2023-03-25 22:33:33 +0100

    ✨ Added timer

--- scrape.py
+++ scrape.py
@@ -2,39 +2,46 @@ import os
 import re
 import httpx
 import threading
+from time import perf_counter
 
 BASE_URL = 'https://xkcd.com'
-HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'}
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'}
 COMIC_FOLDER = 'comics'
 
 
-def get_latest_comic_number(session:httpx.Client) -> int:
+def get_latest_comic_number(session: httpx.Client) -> int:
     ''' Get the latest comic number '''
     res = session.get(BASE_URL)
-    latest_comic_number = re.search(rf'<a href="{BASE_URL}/(.*?)">', res.text).group(1)
-    
+    latest_comic_number = re.search(
+        rf'<a href="{BASE_URL}/(.*?)">', res.text).group(1)
+
     return int(latest_comic_number)
+
 
 def get_latest_local_comic() -> int:
     ''' Get the most recent comic number in the folder '''
-    comics_in_folder = [int(file.split()[0]) for file in os.listdir(COMIC_FOLDER) if file.endswith('.jpg')]
+    comics_in_folder = [int(file.split()[0]) for file in os.listdir(
+        COMIC_FOLDER) if file.endswith('.jpg')]
     most_recent_comic_number = max(comics_in_folder) if comics_in_folder else 0
-    
+
     return most_recent_comic_number
 
-def get_comic_data(session:httpx.Client, comic_num:int) -> dict:
+
+def get_comic_data(session: httpx.Client, comic_num: int) -> dict:
     ''' Get the comic data '''
     res = session.get(f'{BASE_URL}/{comic_num}/info.0.json')
-    
+
     if res.status_code != 200:
         print(f'Skipped #{comic_num} (Status code {res.status_code})')
         return None
-    
+
     return res.json()
+
 
 def download_comic(session, comic_num):
     comic_data = get_comic_data(session, comic_num)
-        
+
     if not comic_data or 'extra_parts' in comic_data:
         print(f'Skipped #{comic_num}')
         return
@@ -48,14 +55,15 @@ def download_comic(session, comic_num):
 
     print(f'Downloaded #{comic_num} ({comic_title})')
 
-def main(): 
-    
+
+def main():
+
     session = httpx.Client(headers=HEADERS, follow_redirects=True)
-    os.makedirs(COMIC_FOLDER, exist_ok = True)
+    os.makedirs(COMIC_FOLDER, exist_ok=True)
 
     latest_comic_number = get_latest_comic_number(session)
     most_recent_comic_number = get_latest_local_comic()
-            
+
     print(f'Latest comic: #{latest_comic_number}')
     print(f'Comics in folder: #{most_recent_comic_number}')
 
@@ -63,11 +71,13 @@ def main():
         print('All comics are up to date')
         session.close()
         return
-    
+
+    start_time = perf_counter()
     # Create threads for downloading comics
     threads = []
     for comic_num in range(most_recent_comic_number + 1, latest_comic_number + 1):
-        thread = threading.Thread(target=download_comic, args=(session, comic_num))
+        thread = threading.Thread(
+            target=download_comic, args=(session, comic_num))
         threads.append(thread)
         thread.start()
 
@@ -75,7 +85,11 @@ def main():
     for thread in threads:
         thread.join()
 
+    end_time = perf_counter()
+    print(f'Downloaded {latest_comic_number - most_recent_comic_number} comics in {end_time - start_time:.2f} seconds. ({(end_time - start_time) / (latest_comic_number - most_recent_comic_number):.2f} seconds per comic')
+
     session.close()
-    
+
+
 if __name__ == '__main__':
     main()
